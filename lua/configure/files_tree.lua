a9a00d41a3c3707c2e69297a42a6f8802f934bd3
cstsunfu <cstsunfu@gmail.com>
2022-04-20 13:41:42 +0800

    update filetree ignore file

--- lua/configure/files_tree.lua
+++ lua/configure/files_tree.lua
@@ -62,59 +62,30 @@ plugin.core = {
         vim.cmd("highlight NvimTreeFolderIcon guifg=#0077aa")
         -- following options are the default
         require'nvim-tree'.setup {
-            nvim_tree_hide_dotfiles = 1,
-            nvim_tree_ignore = { '.git', 'node_modules', '.cache' },
-            nvim_tree_gitignore = 1,
-            nvim_tree_window_picker_exclude = {
-                filetype = {'packer', 'qf'},
-                buftype = {'terminal'}
-            },
-            nvim_tree_disable_window_picker = 1,
-            nvim_tree_quit_on_open = 1,
+            --nvim_tree_hide_dotfiles = 1,
+            --nvim_tree_ignore = { '.git', 'node_modules', '.cache', '$XDG_DATA_HOME'},
+            --nvim_tree_gitignore = 0,
+            --nvim_tree_window_picker_exclude = {
+                --filetype = {'packer', 'qf'},
+                --buftype = {'terminal'}
+            --},
+            --nvim_tree_disable_window_picker = 1,
+            --nvim_tree_quit_on_open = 1,
             -- disables netrw completely
             disable_netrw       = true,
             -- hijack netrw window on startup
             hijack_netrw        = true,
             -- open the tree when running this setup function
             open_on_setup       = false,
-            -- will not open on setup if the filetype is in this list
-            ignore_ft_on_setup  = {},
-            -- closes neovim automatically when the tree is the last **WINDOW** in the view
-            auto_close          = false,
-            -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-            open_on_tab         = false,
-            -- hijack the cursor in the tree to put it at the start of the filename
-            hijack_cursor       = false,
             -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
-            update_cwd          = false,
-            -- show lsp diagnostics in the signcolumn
-            -- lsp_diagnostics     = false,
-            -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-            update_focused_file = {
-                -- enables the feature
-                enable      = false,
-                -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-                -- only relevant when `update_focused_file.enable` is true
-                update_cwd  = false,
-                -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-                -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-                ignore_list = {}
-            },
-            -- configuration options for the system open command (`s` in the tree by default)
-            system_open = {
-                -- the command to run this, leaving nil should work in most cases
-                cmd  = nil,
-                -- the command arguments as a list
-                args = {}
-            },
+            update_cwd          = true,
 
             view = {
                 -- width of the window, can be either a number (columns) or a string in `%`
-                width = "16%",
+                width = "20%",
                 -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
                 side = 'right',
-                -- if true the tree will resize itself after opening a file
-                auto_resize = false,
+                signcolumn = "yes",
                 mappings = {
                     -- custom only false will merge the list with the default mappings
                     -- if true, it will only use your list to set the mappings
@@ -123,13 +94,22 @@ plugin.core = {
                     list = {}
                 }
             },
+            filters = {
+                dotfiles = false,
+                custom = {"$XDG_DATA_HOME", "__pycache__"},
+                exclude = {},
+            },
+            git = {
+                enable = false,
+                ignore = false,
+            },
             diagnostics = {
                 enable = false,
                 icons = {
-                    hint = "",
-                    info = "",
-                    warning = "",
-                    error = "",
+                    hint = " ",
+                    info = " ",
+                    warning = " ",
+                    error = " ",
                 }
             }
         }
--- lua/core/after.lua
+++ lua/core/after.lua
@@ -12,9 +12,9 @@ timer:start(500, 0, vim.schedule_wrap(function()
     --hi StatusLine ctermbg=NONE guibg=dark
 
     vim.cmd("highlight LspDiagnosticsDefaultError guifg='BrightRed'")
-    vim.fn.sign_define("LspDiagnosticsSignError", {text = "", texthl = "LspDiagnosticsSignError"})
-    vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "", texthl = "LspDiagnosticsSignWarning"})
-    vim.fn.sign_define("LspDiagnosticsSignInformation", {text = "", texthl = "LspDiagnosticsSignInformation"})
-    vim.fn.sign_define("LspDiagnosticsSignHint", {text = "ᐅ", texthl = "LspDiagnosticsSignHint"})
+    vim.fn.sign_define("LspDiagnosticsSignError", {text = " ", texthl = "LspDiagnosticsSignError"})
+    vim.fn.sign_define("LspDiagnosticsSignWarning", {text = " ", texthl = "LspDiagnosticsSignWarning"})
+    vim.fn.sign_define("LspDiagnosticsSignInformation", {text = " ", texthl = "LspDiagnosticsSignInformation"})
+    vim.fn.sign_define("LspDiagnosticsSignHint", {text = "ᐅ ", texthl = "LspDiagnosticsSignHint"})
     --vim.cmd("source "..vim.g.CONFIG..'/bug/netrw.vim')
 end))
