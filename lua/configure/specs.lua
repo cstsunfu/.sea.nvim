bf0c521f4f355428a7f3f3b49300225f90b18457
cstsunfu <cstsunfu@gmail.com>
2022-04-26 02:45:51 +0800

    update theme

---
+++ lua/configure/specs.lua
@@ -0,0 +1,45 @@
+local plugin = {}
+
+plugin.core = {
+    'edluffy/specs.nvim',
+    requires = {
+    },
+    setup = function()  -- Specifies code to run before this plugin is loaded.
+
+    end,
+
+    config = function() -- Specifies code to run after this plugin is loaded
+
+        vim.cmd('highlight SpecsH guibg=#666666')
+        require('specs').setup{ 
+            show_jumps  = true,
+            min_jump = 2,
+            popup = {
+                delay_ms = 0, -- delay before popup displays
+                inc_ms = 10, -- time increments used for fade/resize effects 
+                blend = 0, -- starting blend, between 0-100 (fully transparent), see :h winblend
+                width = 12,
+                winhl = "SpecsH",
+                fader = require('specs').empty_fader,
+                resizer = require('specs').shrink_resizer
+            },
+            ignore_filetypes = {},
+            ignore_buftypes = {
+                nofile = true,
+            },
+        }
+    end,
+
+}
+
+plugin.mapping = function()
+    local mappings = require('core.mapping')
+    mappings.register({
+        mode = "n",
+        key = {" ", " "},
+        action = ':lua require("specs").show_specs()<cr>',
+        short_desc = "Blink Cursor",
+        silent = true
+    })
+end
+return plugin
---
+++ lua/core/themes.lua
@@ -0,0 +1,33 @@
+-- there are some my favor colorschemes
+-- set in init.lua
+local theme_configs = {
+    one_light = {
+        'onedarkpro',
+        'light'
+    },
+    --gruvbox_light = { -- use one_light as light theme
+        --'gruvbox_material',
+        --'light'
+    --},
+    gruvbox_dark = {
+        'gruvbox_material',
+        'dark'
+    },
+    nord = {
+        'nord',
+        'dark'
+    },
+    meterial_light = {
+        'nord',
+        'light'
+    },
+    meterial_dark = {
+        'material',
+        'dark'
+    },
+    --vscode_dark = { -- use meterial_dark
+        --'vscode_theme',
+        --'dark'
+    --},
+}
+return theme_configs
