817563a10ffeb75ad9138b290200e4f5b3250f0c
cstsunfu <cstsunfu@gmail.com>
2022-05-01 23:41:39 +0800

    updata task

--- lua/configure/asynctasks.lua
+++ lua/configure/asynctasks.lua
@@ -43,6 +43,34 @@ plugin.mapping = function()
         silent = true,
     })
 
+    mappings.register({
+        mode = "n",
+        key = {"<leader>", "q", "g"},
+        action = nil,
+        short_desc = "Quick Grep",
+    })
+
+    mappings.register({
+        mode = "n",
+        key = {"<leader>", "q", "g", "c"},
+        action = "<cmd>AsyncTask quickfix-rg-grep<cr>",
+        short_desc = "Quick Grep Current Path",
+    })
+
+    mappings.register({
+        mode = "n",
+        key = {"<leader>", "q", "g", "p"},
+        action = "<cmd>AsyncTask quickfix-rg-grep-project<cr>",
+        short_desc = "Quick Grep Project Path",
+    })
+
+    mappings.register({
+        mode = "n",
+        key = {"<leader>", "q", "g", "f"},
+        action = "<cmd>AsyncTask quickfix-rg-grep-filetype<cr>",
+        short_desc = "Quick Grep Current Path File Types",
+    })
+
 
 end
 return plugin
