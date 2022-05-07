dda290c853b7f79fbc1b536821f0e0f34c4b85a3
cstsunfu <cstsunfu@gmail.com>
2022-05-07 16:29:21 +0800

    disable useless vimwiki map

--- lua/configure/vimwiki.lua
+++ lua/configure/vimwiki.lua
@@ -8,10 +8,10 @@ plugin.core = {
         vim.g.vimwiki_map_prefix = '<leader>O'
         vim.g.vimwiki_key_mappings = {
             all_maps = 1,
-            global = 1,
-            headers = 1,
-            text_objs = 1,
-            table_format = 1,
+            global = 0,
+            headers = 0,
+            text_objs = 0,
+            table_format = 0,
             table_mappings = 0,
             lists = 1,
             links = 1,
@@ -23,38 +23,38 @@ plugin.core = {
     config = function() -- Specifies code to run after this plugin is loaded
         vim.cmd("let g:vimwiki_list = [{'path': $HOME.'/org/', 'syntax': 'markdown', 'ext': '.md'}]")
         local global_func = require('util.global')
-        global_func.augroup('Vimwiki Map', {
-            {
-                events = { 'Filetype' },
-                targets = { 'vimwiki' },
-                command = "vnoremap <unique><silent> <leader><cr> :call VVimwiki_create_dir()<cr>"
-            },
-            {
-                events = { 'Filetype' },
-                targets = { 'vimwiki' },
-                command = 'inoremap <silent><expr><buffer> <cr> pumvisible() ? coc#_select_confirm() : "<C-]><Esc>:VimwikiReturn 1 5<CR>"'
-            },
-            {
-                events = { 'Filetype' },
-                targets = { 'vimwiki' },
-                command = "nnoremap <unique><silent> <leader><cr> :call NVimwiki_create_dir()<cr>"
-            },
-            {
-                events = { 'Filetype' },
-                targets = { 'vimwiki' },
-                command = "inoremap <silent><expr><buffer> <C-j> vimwiki#tbl#kbd_tab()"
-            },
-            {
-                events = { 'Filetype' },
-                targets = { 'vimwiki' },
-                command = "inoremap <silent><expr><buffer> <C-k> vimwiki#tbl#kbd_shift_tab()"
-            },
-            {
-                events = { 'Filetype' },
-                targets = { 'vimwiki' },
-                command = "nnoremap <localleader>tt :VimwikiToggleListItem<cr>"
-            },
-        })
+        --global_func.augroup('Vimwiki Map', {
+        --    {
+        --        events = { 'Filetype' },
+        --        targets = { 'vimwiki' },
+        --        command = "vnoremap <unique><silent> <leader><cr> :call VVimwiki_create_dir()<cr>"
+        --    },
+        --    {
+        --        events = { 'Filetype' },
+        --        targets = { 'vimwiki' },
+        --        command = 'inoremap <silent><expr><buffer> <cr> pumvisible() ? coc#_select_confirm() : "<C-]><Esc>:VimwikiReturn 1 5<CR>"'
+        --    },
+        --    {
+        --        events = { 'Filetype' },
+        --        targets = { 'vimwiki' },
+        --        command = "nnoremap <unique><silent> <leader><cr> :call NVimwiki_create_dir()<cr>"
+        --    },
+        --    {
+        --        events = { 'Filetype' },
+        --        targets = { 'vimwiki' },
+        --        command = "inoremap <silent><expr><buffer> <C-j> vimwiki#tbl#kbd_tab()"
+        --    },
+        --    {
+        --        events = { 'Filetype' },
+        --        targets = { 'vimwiki' },
+        --        command = "inoremap <silent><expr><buffer> <C-k> vimwiki#tbl#kbd_shift_tab()"
+        --    },
+        --    {
+        --        events = { 'Filetype' },
+        --        targets = { 'vimwiki' },
+        --        command = "nnoremap <localleader>tt :VimwikiToggleListItem<cr>"
+        --    },
+        --})
     end,
 }
 
