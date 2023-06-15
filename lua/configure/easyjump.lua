a76d3210739d74ed1347309645bc006b81b684f8
cstsunfu <cstsunfu@gmail.com>
2023-06-15 22:24:05 +0800

    dev: use `s` in normal mode to triggle easyjump

--- lua/configure/easyjump.lua
+++ lua/configure/easyjump.lua
@@ -2,6 +2,7 @@ local plugin = {}
 
 plugin.core = {
     "cstsunfu/pounce-zh.nvim",
+    cmd = { "Pounce" },
     init = function() -- Specifies code to run before this plugin is loaded.
     end,
 
