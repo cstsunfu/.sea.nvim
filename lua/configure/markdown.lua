e41da21c55d908bfda185fc023dcec40f27f5323
cstsunfu <cstsunfu@gmail.com>
2024-04-23 22:43:10 +0800

    fix: disable the vim-markdown default fold

--- lua/configure/markdown.lua
+++ lua/configure/markdown.lua
@@ -21,6 +21,7 @@ plugin.core = {
             remove = true,
         })
 
+        vim.g.vim_markdown_folding_disabled = 1
         vim.g.vim_markdown_conceal = 1
         vim.g.vim_markdown_frontmatter = 1
         vim.g.vim_markdown_conceal_code_blocks = 0
